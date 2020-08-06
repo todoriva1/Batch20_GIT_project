f1b9e9e1a5e98dcf0eb525132aa4893f5fee3af6
todoriva1 <todoriva@hotmail.com>
2020-08-05 21:23:44 -0400

    add MissJava.java

---
+++ src/Utility/MissJava.java
@@ -0,0 +1,11 @@
+package Utility;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class MissJava {
+
+    // I miss Daria so much !!
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
