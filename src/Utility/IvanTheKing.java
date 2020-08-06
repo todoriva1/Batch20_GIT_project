bcb6263bc830ec56affcaace4015c7e7749b038c
todoriva1 <todoriva@hotmail.com>
2020-08-05 20:56:20 -0400

    add Ivan Kralj

---
+++ src/Utility/IvanTheKing.java
@@ -0,0 +1,12 @@
+package Utility;
+
+public class IvanTheKing {
+    public static void main(String[] args) {
+
+        String i = "Ivan";
+        String k = "kralj";
+        // :)
+        System.out.println(i+" "+k);
+
+    }
+}
